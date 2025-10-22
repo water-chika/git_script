a5d26cf8748878320fc28c0ecaad3e90a0822ba9
Water Chika <water_chika@outlook.com>
2025-10-22 10:11:37 +0800

    git_repo defaultly recursive

--- git_repo.py
+++ git_repo.py
@@ -123,7 +123,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('url', type=str)
     parser.add_argument('--worktree', type=str, default=None)
-    parser.add_argument('--recursive', type=bool, default=False)
+    parser.add_argument('--recursive', type=bool, default=True)
     args = parser.parse_args()
 
     config = None
