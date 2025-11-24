acab42425ac147da4b540a69d8a4b5e5eb5b8104
Water Chika <water_chika@outlook.com>
2025-11-25 03:48:39 +0800

    try to cache build result for git commit

---
+++ git_build.py
@@ -0,0 +1,29 @@
+import subprocess
+
+def build():
+    subprocess.run([
+        'cmake', '-S', '.', '-B', 'build'
+        ])
+    subprocess.run([
+        'cmake', '--build', 'build'
+        ])
+    return [
+            pathlib.Path('build/bin/test-backends-ops').Absolute()
+            ]
+
+def main():
+    out = subrocess.run([
+        'git', 'rev-parse', 'HEAD'
+        ], capture_output=True, encoding='utf-8')
+    commit = out.stdout
+    cache_dir = pathlib.Path('/mnt/builds/llama.cpp')
+    cache = cache_dir / commit
+    if not cache.exist():
+        cached_files = build()
+        for file in cached_files:
+            file.copy_to(cache_dir)
+    cache.copy_to('build/bin')
+
+    pass
+if __name__ == '__main__':
+    main()
