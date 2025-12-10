88eb3e2b6c476a611825f6de744f5e2a9b4da7e6
Water Chika <water_chika@outlook.com>
2025-12-10 13:42:24 +0800

    update git_build

--- git_build.py
+++ git_build.py
@@ -1,13 +1,22 @@
+#!/usr/bin/env python
+
 import subprocess
 import pathlib
 from git_repo import load_config
+import argparse
+import os
 
-def build():
-    subprocess.run([
-        'cmake', '-S', '.', '-B', 'build'
+def subprocess_run(cmds):
+    print(cmds)
+    return subprocess.run(cmds)
+
+def build(build_dir):
+    build_dir.mkdir(exist_ok=True)
+    subprocess_run([
+        'cmake', '-S', '.', '-B', build_dir
         ])
-    subprocess.run([
-        'cmake', '--build', 'build'
+    subprocess_run([
+        'cmake', '--build', build_dir
         ])
 
 def git_build():
@@ -26,7 +35,22 @@ def git_build():
     cache.copy_into('build/bin')
 
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--commit', type=str, default='')
+    parser.add_argument('--worktree', type=str, default='.')
+    parser.add_argument('command', type=str)
+    args = parser.parse_args()
+
+    config = load_config()
+    builds_dir = pathlib.Path(config['build_dir']).absolute()
+    build_dir = builds_dir / pathlib.Path.cwd().name
+
+    if (args.command == 'build'):
+        build(build_dir)
+    elif (args.command == 'path'):
+        print(build_dir)
+    else:
+        raise LookupError(args.command)
 
 if __name__ == '__main__':
     main()
