994f48ac3e5b99895f12fd50728af7c784127f05
Water Chika <water_chika@outlook.com>
2026-06-09 15:30:02 +0800

    refine git proxy

--- git.cpp
+++ git.cpp
@@ -6,62 +6,20 @@
 #include <optional>
 #include <cstring>
 
-std::optional<std::filesystem::path> get_next_command(std::string command) {
-    auto path = std::getenv("PATH");
-    char sep = PATH_SEPERATOR;
+#if WIN32
+#include <process.h>
+#define GIT_PATH "C:/Program Files/Git/git"
+#else
+#include <unistd.h>
+#define GIT_PATH "/usr/bin/git"
+#endif
 
-    auto first_command = true;
-    auto prev_path = path;
-    while (*path != '\0') {
-        if (*path == sep) {
-            auto command_path = std::filesystem::path{prev_path, path} / command;
-            prev_path = path+1;
-            if (first_command) {
-                first_command = false;
-            }
-            else if (exists(command_path)) {
-                return command_path;
-            }
-        }
-        ++path;
-    }
-    if (prev_path != path) {
-            auto command_path = std::filesystem::path{prev_path, path} / command;
-            prev_path = path+1;
-            if (first_command) {
-                first_command = false;
-            }
-            else if (exists(command_path)) {
-                return command_path;
-            }
-    }
-    return std::nullopt;
-}
-
-int main(int argc, const char* argv[]) {
-    std::string cmd = "";
-
-    auto git_path = get_next_command("git");
-
-    if (git_path == std::nullopt) {
-        std::cerr << "not found next git";
-        return -1;
-    }
-
+int main(int argc, char* const argv[]) {
     if (argc != 3 || strcmp(argv[1], "clone") != 0 || argv[2][0] == '-') {
-        cmd = absolute(git_path.value());
-        for (int i = 1; i < argc; i++) {
-            cmd += " ";
-            cmd += argv[i];
-        }
+        execv(GIT_PATH, argv);
     }
     else {
-        cmd = "python " GIT_REPO_PY_PATH;
-        cmd += " --git_path ";
-        cmd += absolute(git_path.value());
-        cmd += " ";
-        cmd += argv[2];
+        execl("python", GIT_REPO_PY_PATH, argv[2]);
     }
-    system(cmd.c_str());
     return 0;
 }
