b26f1a20a63b2dbd82189913a11b4f8cab298da1
Water Chika <water_chika@outlook.com>
2026-06-17 15:04:35 +0800

    git proxy: fix typo

--- git.cpp
+++ git.cpp
@@ -18,7 +18,7 @@
 
 int main(int argc, char* const argv[]) {
     if (argc == 3 && strcmp(argv[1], "clone") == 0 && argv[2][0] != '-') {
-        return execl(PYTHON_PATH, PYTHON_PATH, "/home/water/worktrees/git_script_build/git_script/git_repo.py", argv[2], NULL);
+        return execl(PYTHON_PATH, PYTHON_PATH, GIT_REPO_PY_PATH, argv[2], NULL);
     }
     else if (argc == 4 && strcmp(argv[1], "clone") == 0 && argv[2][0] != '-' && argv[3][0] != '-') {
         return execl(PYTHON_PATH, PYTHON_PATH, GIT_REPO_PY_PATH, argv[2], "--worktree", argv[3], NULL);
