d67e0d9fc632e9d17167d845fee207e3380bafcf
Water Chika <water_chika@outlook.com>
2026-06-17 16:35:34 +0800

    wait process

--- git.cpp
+++ git.cpp
@@ -9,20 +9,35 @@
 #if WIN32
 #include <process.h>
 #define GIT_PATH "C:/Program Files/Git/cmd/git.exe"
+template<typename... Argv>
+int exec_l(const char* path, Argv... argv){
+    return spawnl(_P_WAIT, path, argv..., NULL);
+}
+int exec_v(const char* path, char* argv[]) {
+    return spawnv(_P_WAIT, path, argv);
+}
 #else
 #include <unistd.h>
 #define GIT_PATH "/usr/bin/git"
+
+template<typename... Argv>
+int exec_l(const char* path, Argv... argv){
+    return execl(path, argv..., NULL);
+}
+int exec_v(const char* path, char** argv) {
+    return execv(path, argv);
+}
 #endif
 
-int main(int argc, char* const argv[]) {
+int main(int argc, char* argv[]) {
     if (argc == 3 && strcmp(argv[1], "clone") == 0 && argv[2][0] != '-') {
-        return execl(PYTHON_PATH, PYTHON_PATH, GIT_REPO_PY_PATH, argv[2], NULL);
+        return exec_l(PYTHON_PATH, PYTHON_PATH, GIT_REPO_PY_PATH, argv[2]);
     }
     else if (argc == 4 && strcmp(argv[1], "clone") == 0 && argv[2][0] != '-' && argv[3][0] != '-') {
-        return execl(PYTHON_PATH, PYTHON_PATH, GIT_REPO_PY_PATH, argv[2], "--worktree", argv[3], NULL);
+        return exec_l(PYTHON_PATH, PYTHON_PATH, GIT_REPO_PY_PATH, argv[2], "--worktree", argv[3]);
     }
     else {
-        return execv(GIT_PATH, argv);
+        return exec_v(GIT_PATH, argv);
     }
     return 0;
 }
