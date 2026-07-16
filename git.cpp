e2b17d1cfe194a33c2fdb68c368f58c0ea3325bb
Water Chika <water_chika@outlook.com>
2026-07-16 16:37:03 +0800

    add fork_exec_lp

--- git.cpp
+++ git.cpp
@@ -5,23 +5,97 @@
 #include <regex>
 #include <optional>
 #include <cstring>
+#include <cassert>
 
 #if WIN32
 #include <process.h>
 #define GIT_PATH "C:/Program Files/Git/cmd/git.exe"
+std::string process_escape_character(const char* str) {
+    std::string res{};
+    while (*str != '\0') {
+        if (*str == '"' || *str == '\'') {
+            res.push_back('\\');
+        }
+        res.push_back(*str);
+        str++;
+    }
+    return res;
+}
 template<typename... Argv>
 int exec_l(const char* path, Argv... argv){
-    return spawnl(_P_WAIT, path, argv..., NULL);
+    auto args = std::vector<const char*>{argv...};
+    std::vector<std::string> processed_args{};
+    for (auto& arg : args) {
+        processed_args.push_back(
+            process_escape_character(arg)
+        );
+        arg = processed_args.back().data();
+    }
+    args.push_back(NULL);
+    return spawnv(_P_WAIT, path, args.data());
 }
 int exec_v(const char* path, char* argv[]) {
-    return spawnv(_P_WAIT, path, argv);
+    std::vector<std::string> processed_args{};
+    auto p = argv;
+    while (*p != nullptr) {
+        auto& arg = *p;
+        p++;
+        processed_args.push_back(
+            process_escape_character(arg)
+        );
+    }
+    std::vector<char*> args(processed_args.size()+1);
+    std::transform(processed_args.begin(), processed_args.end(),
+        args.begin(),
+        [](auto& s) { return s.data(); }
+    );
+    args.back() = NULL;
+    return spawnv(_P_WAIT, path, args.data());
 }
 template<typename... Argv>
 int exec_lp(const char* path, Argv... argv){
-    return spawnlp(_P_WAIT, path, argv..., NULL);
+    auto args = std::vector<const char*>{argv...};
+    std::vector<std::string> processed_args{};
+    for (auto& arg : args) {
+        processed_args.push_back(
+            process_escape_character(arg)
+        );
+        arg = processed_args.back().data();
+    }
+    args.push_back(NULL);
+    assert(args.size() > 1);
+    return spawnvp(_P_WAIT, path, args.data());
 }
 int exec_vp(const char* path, char* argv[]) {
-    return spawnvp(_P_WAIT, path, argv);
+    std::vector<std::string> processed_args{};
+    auto p = argv;
+    while (*p != nullptr) {
+        auto& arg = *p;
+        p++;
+        processed_args.push_back(
+            process_escape_character(arg)
+        );
+    }
+    std::vector<char*> args(processed_args.size()+1);
+    std::transform(processed_args.begin(), processed_args.end(),
+        args.begin(),
+        [](auto& s) { return s.data(); }
+    );
+    args.back() = NULL;
+    return spawnvp(_P_WAIT, path, args.data());
+}
+template<typename... Argv>
+int fork_exec_lp(const char* path, Argv... argv) {
+    auto args = std::vector<const char*>{argv...};
+    std::vector<std::string> processed_args{};
+    for (auto& arg : args) {
+        processed_args.push_back(
+            process_escape_character(arg)
+        );
+        arg = processed_args.back().data();
+    }
+    args.push_back(NULL);
+    return spawnvp(_P_WAIT, path, args.data());
 }
 #else
 #include <unistd.h>
@@ -42,6 +116,25 @@ int exec_lp(const char* path, Argv... argv){
 int exec_vp(const char* path, char** argv) {
     return execvp(path, argv);
 }
+template<typename... Argv>
+int fork_exec_lp(const char* path, Argv... argv) {
+    pid_t ret = fork();
+    if (ret == 0) {
+        exec_lp(path, argv...);
+        exit(-1); // This should not be called.
+    }
+    else {
+        if (ret == -1) {
+            std::cerr << "fork failed" << std::endl;
+            return -1;
+        }
+        else {
+            int wstatus = 0;
+            wait(&wstatus);
+            return wstatus;
+        }
+    }
+}
 #endif
 
 int main(int argc, char* argv[]) {
@@ -54,21 +147,8 @@ int main(int argc, char* argv[]) {
     else if (argc == 4 && strcmp(argv[1], "submodule") == 0 &&
             strcmp(argv[2], "add") == 0 &&
             argv[3][0] != '-') {
-        pid_t ret = fork();
-        if (ret == 0) {
-            return exec_lp(PYTHON_PATH, PYTHON_PATH, GIT_REPO_PY_PATH, argv[3]);
-        }
-        else {
-            if (ret == -1) {
-                std::cerr << "fork failed" << std::endl;
-                return -1;
-            }
-            else {
-                int wstatus = 0;
-                wait(&wstatus);
-            }
-            return exec_lp(GIT_PATH, GIT_PATH, "submodule", "add", argv[3]);
-        }
+        auto ret = fork_exec_lp(PYTHON_PATH, PYTHON_PATH, GIT_REPO_PY_PATH, argv[3]);
+        return exec_lp(GIT_PATH, GIT_PATH, "submodule", "add", argv[3]);
     }
     else if (argc == 3 && strcmp(argv[1], "init") == 0 && argv[2][0] != '-') {
         return exec_lp(PYTHON_PATH, PYTHON_PATH, GIT_REPO_PY_PATH, "--init", "--worktree", argv[2]);
